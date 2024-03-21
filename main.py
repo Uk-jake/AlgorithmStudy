80667b3d68c6f236ba3dc19269e90b589df02629
Uk-jake <mag0225@naver.com>
2024-03-21 09:28:49 +0900

    joonlab1095

--- main.py
+++ main.py
@@ -1,45 +1,26 @@
-import queue
 #Solution
-def solution(n, S):
+def solution(A, B):
 
-    que1 = queue.Queue()
-    que2 = queue.Queue()
-    A,B,C = [],[],[]
+    C = []
 
-    for item in S:
-        if item[0] == 1:
-            que1.put(item[1])
-            que2.put(item[2])
-        elif item[0] == 2:
-            # item[1] - 준비된 음식 종류
-            number = que1.get()
-            want = que2.get()
-            if item[1] == want:
-                A.append(number)
-            else :
-                B.append(number)
-
-    if que1.qsize() > 0:
-        for i in range(que1.qsize()):
-            C.append(que1.get())
-        C.sort()
-    elif que1.qsize() == 0:
-        C.append("None")
-
-    A.sort()
-    B.sort()
-
-    for a in A:
-        print(f"{a}", end=" ")
-    print("")
     for b in B:
-        print(f"{b}", end=" ")
-    print("")
+        count = 0
+        for a in A:
+            if b <= a:
+                count += 1
+        C.append(count)
+
     for c in C:
-        print(f"{c}", end=" ")
+        print(c)
+
+
 
 # input
-n = int(input())
-S = list(list(map(int, input().strip().split())) for _ in range(n))
+n, m = map(int,input().strip().split())
+A = list(map(int,input().strip().split()))
+#B = []
+#for i in range(m):
+#    B.append(int(input().strip()))
+B = list(int(input().strip()) for _ in range(m))
 
-solution(n,S)
+solution(A,B)
