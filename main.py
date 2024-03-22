664f1bc027696f063352dc9db93995e5f5b54c5f
Uk-jake <mag0225@naver.com>
2024-03-22 11:54:09 +0900

    joonlab1099

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
-#Solution
+# Solution
 def solution(A, B):
-
-    C = []
-
     for b in B:
         if b[0] == 1:
-            for i in range(b[1],b[2]+1):
-                A[i] += b[3]
+            add_k(A, b[1], b[3], b[2], b[4], b[5])
         elif b[0] == 2:
             count = 0
-            for a in A[b[1]:b[2]+1]:
-                count += a
-            C.append(count)
+            for i in range(b[1], b[3] + 1):
+                for j in range(b[2], b[4] + 1):
+                    count += A[i][j]
+            print(count)
 
-    return C
 
+def add_k(A, a, b, c, d, k):
+    for i in range(a, b + 1):
+        for j in range(c, d + 1):
+            A[i][j] += k
 
 # input
-n , m = map(int, input().strip().split())
-A = list(map(int,input().strip().split()))
-B = list(list(map(int,input().strip().split())) for _ in range(m))
+n, m = map(int, input().strip().split())
+A = list(list(map(int, input().strip().split())) for _ in range(n))
+B = list(list(map(int, input().strip().split())) for _ in range(m))
 
 # print(n, m)
 # print(A)
 # print(B)
 
-answer = solution(A,B)
-
-for item in answer:
-    print(item)
+solution(A, B)
