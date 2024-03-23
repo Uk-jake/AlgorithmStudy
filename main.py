5981f770de9f246709ad43a933d8bd192be72403
Uk-jake <mag0225@naver.com>
2024-03-23 15:00:15 +0900

    joonlab1108

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 # Solution
-def solution(S):
-    return sorted(S)
+def solution(A):
+
+    time = [0] * 3600
+
+    for a in A:
+        if a[0] == '1':
+            t1 = translate_time(a[1])
+            t2 = translate_time(a[2])
+            for i in range(t1, t2):
+                time[i] += 1
+
+        elif a[0] == '2':
+            t1 = translate_time(a[1])
+            print(time[t1])
+
+    return 0
+
+def translate_time(t):
+    return int(t[:2])*60 + int(t[3:])
 
 # input
-S = input().strip()
-print(S, type(S))
+n = int(input().strip())
+A = list(input().strip().split() for _ in range(n))
 
-# sorted() - 파이썬 표준 내장 함수 : 정렬
-#   새로운 정렬 결과 반환, 원래 목록에 영향 X
-
-# sort() - 리스트 메서드 : 정렬 -> str형태에는 사용할 수 없음.
-#   기존 리스트의 값들을 정렬해주는 기능, 반환 값 없음.
-answer = sorted(S)
-
-for s in answer:
-    print(f"{s}" , end="")
+solution(A)
