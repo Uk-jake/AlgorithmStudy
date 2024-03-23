7d108452b5090ad03486e37008028f94fc70f446
Uk-jake <mag0225@naver.com>
2024-03-23 13:48:03 +0900

    joonlab1103

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 # Solution
-def solution(A, B):
-    for b in B:
-        if b[0] == 1:
-            add_k(A, b[1], b[3], b[2], b[4], b[5])
-        elif b[0] == 2:
-            count = 0
-            for i in range(b[1], b[3] + 1):
-                for j in range(b[2], b[4] + 1):
-                    count += A[i][j]
-            print(count)
+def solution(S):
+    # 'a' 또는 'A'가 두번 이상 연속되는 부분 문자열을 찾아 'a'로 치환한다.
 
+    T = ""
+    i = 0
 
-def add_k(A, a, b, c, d, k):
-    for i in range(a, b + 1):
-        for j in range(c, d + 1):
-            A[i][j] += k
+    # while 문을 이용하여 문자열 S의 원소 하나씩 탐색
+    while i < len(S):
+        # 탐색하는 원소가 'a' , 'A'가 아니라면 문자열 T에 추가
+        if S[i] != 'a' and S[i] != 'A':
+            T += S[i]
+            i += 1
+            continue
+
+        # 탐색하던 원소가 'a' , 'A'면 그 뒤에 부분문자열이 얼마나 있는지 탐색
+        j = i + 1
+
+        while j < len(S):
+            # i번째 원소 뒤로 'a', 'A'가 아닌 문자가 있을 떄까지
+            if S[j] != 'a' and S[j] != 'A':
+                break
+            j += 1
+
+        #뒤에 다른 문자열이 있다면 바로 추가
+        if j - i == 1:
+            T += S[i]
+        else:
+            T += S[i].lower()
+
+        i = j
+
+    return T
 
 # input
-n, m = map(int, input().strip().split())
-A = list(list(map(int, input().strip().split())) for _ in range(n))
-B = list(list(map(int, input().strip().split())) for _ in range(m))
-
-# print(n, m)
-# print(A)
-# print(B)
-
-solution(A, B)
+S = input().strip()
+print(solution(S))
